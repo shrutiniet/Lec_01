d843167e3dbcdebc7cfdeedd7033caddf775b4d2
Shruti Sharma <69854848+shrutiniet@users.noreply.github.com>
2022-11-14 16:21:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Lec_01;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Integer ="+n);
+		String str=sc.next();
+		System.out.println("String= "+str);
+		char ch=sc.next().charAt(0);
+		System.out.println("Charater= "+ch);
+		float f=sc.nextFloat();
+		System.out.println("Float= "+f);
+
+	}
+
+}
